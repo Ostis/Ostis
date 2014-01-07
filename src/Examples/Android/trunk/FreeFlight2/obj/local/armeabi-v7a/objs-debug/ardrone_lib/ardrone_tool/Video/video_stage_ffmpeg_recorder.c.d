cb4e800ad04da7ee45936f70bd81c3ba782c78ac
Nicolas Schurando <nicolas.schurando@ece.fr>
2014-01-07 10:38:19 -0800

    Added gitignore file for the Android example project.

---
+++ src/Examples/Android/trunk/FreeFlight2/obj/local/armeabi-v7a/objs-debug/ardrone_lib/ardrone_tool/Video/video_stage_ffmpeg_recorder.c.d
@@ -0,0 +1,2 @@
+video_stage_ffmpeg_recorder.o ../../Soft/Build/targets_versions/ardrone_lib_DEBUG_MODE_vlib_Linux_3.8.0-35-generic_GNU_Linux_homenowan190android-ndksandroid-ndk-r9ctoolchainsarm-linux-androideabi-4.6prebuiltlinux-x86binarm-linux-androideabi-gcc_4.6/ardrone_tool/Video/video_stage_ffmpeg_recorder.c.d: \
+ ../..//Soft/Lib/ardrone_tool/Video/video_stage_ffmpeg_recorder.c
