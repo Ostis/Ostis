08129e5ab3bd05885fc3680faf8fbd8d245ec2bd
Nicolas Schurando <nicolas.schurando@ece.fr>
2014-01-07 10:07:08 -0800

    Fixed build settings.

--- src/Examples/Android/trunk/FreeFlight2/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/nowan190/git/Ostis/src/Examples/Android/trunk/FreeFlight2/bin/AndroidManifest.xml : \
-/home/nowan190/git/Ostis/src/Examples/Android/trunk/FreeFlight2/AndroidManifest.xml \
--- src/Examples/Android/trunk/FreeFlight2/obj/local/armeabi-v7a/objs-debug/sdk/ATcodec/ATcodec_Memory.c.d
+++
@@ -1,6 +0,0 @@
-ATcodec_Memory.o ../../Soft/Build/targets_versions/sdk_DEBUG_MODE_vlib_Linux_3.8.0-35-generic_GNU_Linux_homenowan190android-ndksandroid-ndk-r9ctoolchainsarm-linux-androideabi-4.6prebuiltlinux-x86binarm-linux-androideabi-gcc_4.6/ATcodec/ATcodec_Memory.c.d: ..//ATcodec/ATcodec_Memory.c ../VP_Os/vp_os_assert.h \
- /home/nowan190/android-ndks/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/assert.h \
- /home/nowan190/android-ndks/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
- /home/nowan190/android-ndks/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
- /home/nowan190/android-ndks/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/android/api-level.h \
- ../ATcodec/ATcodec_Memory.h
