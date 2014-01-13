d1f8c8bd34a3f1911862472f2c296411368765a4
Paul Bouillon <paul.bouillon@ece.fr>
2014-01-13 16:30:52 +0100

    Added lang and actions packages

---
+++ src/OstisAndroidApp/src/fr/ece/ostis/lang/Language.java
@@ -0,0 +1,11 @@
+package fr.ece.ostis.lang;
+
+public class Language {
+	public static final int NUMBER = 2;
+	
+	public static final int FRENCH = 0;
+    public static final int ENGLISH = 1;
+    
+    public static final int DEFAULT = ENGLISH;
+    public static int Value = DEFAULT;
+}
