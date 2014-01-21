c55e689d9d3b6e8bc27276331b31247fd738cadd
Nicolas Schurando <nicolas.schurando@ece.fr>
2014-01-21 05:47:14 -0800

    Added minimal implementation of the AR.Drone SDK.

---
+++ src/OstisAndroidApp/src/com/parrot/freeflight/drone/DroneAcademyMediaListener.java
@@ -0,0 +1,7 @@
+package com.parrot.freeflight.drone;
+
+public interface DroneAcademyMediaListener{
+    void onNewMediaIsAvailable(String path);
+    void onNewMediaToQueue(String path);
+    void onQueueComplete();
+}
