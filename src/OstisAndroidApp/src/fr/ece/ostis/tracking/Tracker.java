8628d846e2a46119bd7464c1e5feabb12934d2a5
paulkub <bouillon@ece.fr>
2014-02-05 12:56:49 +0100

    Updated Tracker.

--- src/OstisAndroidApp/src/fr/ece/ostis/tracking/Tracker.java
+++ src/OstisAndroidApp/src/fr/ece/ostis/tracking/Tracker.java
@@ -17,8 +17,8 @@ import com.googlecode.javacv.cpp.opencv_imgproc.CvMoments;
  */
 public class Tracker {
 	
-    static int hueLowerR = 0;
-    static int hueUpperR = 45;
+    static int hueLowerR = 160;
+    static int hueUpperR = 180;
     
     public static final int AR_VIDEO_WIDTH = 320;
     public static final int AR_VIDEO_HEIGHT = 240;
